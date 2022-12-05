37788f767f0073fda516d6e530bda0121b78e1f4
Nikola Simonovic <nikolasimonovic94@gmail.com>
2022-12-05 13:52:21 +0100

    ...

--- day5/p1.js
+++ day5/p1.js
@@ -16,9 +16,7 @@ for(let i = 0, j = 0; i < inputs[0].length; i += 4, j++) {
 }
 
 for(let i = inputs.findIndex(x => x === "") + 1; i < inputs.length; i++) {
-    let moveCount = +inputs[i].split(" ")[1];
-    let moveFrom = inputs[i].split(" ")[3] - 1;
-    let moveTo = inputs[i].split(" ")[5] - 1;
+    let [moveCount, moveFrom, moveTo] = [+inputs[i].split(" ")[1], inputs[i].split(" ")[3] - 1, inputs[i].split(" ")[5] - 1];
     for(let j = 0; j < moveCount; j++) {
         crates[moveTo].unshift(crates[moveFrom][0]);
         crates[moveFrom].shift();
--- day5/p2.js
+++ day5/p2.js
@@ -16,9 +16,7 @@ for(let i = 0, j = 0; i < inputs[0].length; i += 4, j++) {
 }
 
 for(let i = inputs.findIndex(x => x === "") + 1; i < inputs.length; i++) {
-    let moveCount = +inputs[i].split(" ")[1];
-    let moveFrom = inputs[i].split(" ")[3] - 1;
-    let moveTo = inputs[i].split(" ")[5] - 1;
+    let [moveCount, moveFrom, moveTo] = [+inputs[i].split(" ")[1], inputs[i].split(" ")[3] - 1, inputs[i].split(" ")[5] - 1];
     for(let j = moveCount - 1; j >= 0; j--) {
         crates[moveTo].unshift(crates[moveFrom][j]);
         crates[moveFrom].splice(j, 1);
