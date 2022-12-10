353547a4b1c2fee969ebd5acf0d5bbe9d5eaf1ac
Nikola Simonovic <nikolasimonovic94@gmail.com>
2022-12-11 00:11:21 +0100

    ...

--- day10/p2.js
+++ day10/p2.js
@@ -3,20 +3,18 @@ const data = fs.readFileSync(`./input.txt`, "utf-8");
 
 const inputs = data.split("\r\n");
 
-let cycle = 1, x = 1;
-let row = "";
+let cycle = 0, x = 1, row = "";
 
 for(const line of inputs) {
     const loops = line.startsWith("addx") ? 2 : 1;
 
-    for(let i = 0; i < loops; i++) {
-        const column = (cycle - 1) % 40;
+    for(let i = 0; i < loops; i++, cycle++) {
+        const column = cycle % 40;
         row += x - 1 <= column && column <= x + 1 ? '█' : ' ';
         if(column === 39) {
             console.log(row);
             row = "";
         }
-        cycle++;
     }
 
     x += loops === 2 ? +line.split(" ")[1] : 0;
