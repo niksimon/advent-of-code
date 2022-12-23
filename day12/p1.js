0522fff12de97895bfbcff21e8ee8fd17784df07
Nikola Simonovic <nikolasimonovic94@gmail.com>
2022-12-23 01:05:23 +0100

    ...

--- day12/p1.js
+++ day12/p1.js
@@ -3,113 +3,67 @@ const data = fs.readFileSync(`./input.txt`, "utf-8");
 
 const inputs = data.split("\r\n");
 
-function findShortestPath(startPos, endPos, grid) {
-    let queue = [new Position(startPos.top, startPos.left, [], "Start")];
+const map = [];
+const mapWidth = inputs[0].length;
+const mapHeight = inputs.length;
+const visited = new Set();
+const start = {};
+const end = {};
 
-    while (queue.length > 0) {
-        const currentPos = queue.shift();
-        const directions = ["Up", "Down", "Left", "Right"];
+for (let i = 0; i < mapHeight; i++) {
+    map[i] = [];
+    for (let j = 0; j < mapWidth; j++) {
+        let char = inputs[i][j];
+        if(char === "S") {
+            Object.assign(start, {top: i, left: j});
+            char = "a";
+        }
+        else if(char === "E") {
+            Object.assign(end, {top: i, left: j});
+            char = "z";
+        }
+        map[i][j] = char;
+    }
+}
 
-        for (const dir of directions) {
-            let newPos = exploreNeighbour(currentPos, endPos, dir, grid);
-            if (newPos.status === "End") {
-                return newPos.path;
-            } else if (newPos.status === "Valid") {
-                grid[newPos.top][newPos.left].visited = true;
-                queue.push(newPos);
+console.log(bfs(start, end, map).length);
+
+function bfs(start, end, map) {
+    const q = [{
+        top: start.top,
+        left: start.left,
+        path: []
+    }];
+    const directions = [[0, -1], [0, 1], [-1, 0], [1, 0]];
+
+    while(q.length > 0) {
+        const currentPos = q.shift();
+        for(const d of directions) {
+            const path = currentPos.path.slice();
+            let top = currentPos.top;
+            let left = currentPos.left;
+
+            top += d[1];
+            left += d[0];
+
+            path.push([top, left]);
+
+            if(top >= 0 && top < mapHeight && left >= 0 && left < mapWidth && 
+                (map[top][left].charCodeAt() - map[currentPos.top][currentPos.left].charCodeAt() <= 1)) {
+                if(top === end.top && left === end.left) {
+                    return path;
+                }
+                else if(!visited.has(`${top},${left}`)) {
+                    q.push({
+                        top: top,
+                        left: left,
+                        path: path
+                    });
+                    visited.add(`${top},${left}`);
+                }
             }
         }
     }
 
     return false;
 }
-
-function exploreNeighbour(currentPos, endPos, direction, grid) {
-    let newPath = currentPos.path.slice();
-    newPath.push(direction);
-
-    let top = currentPos.top;
-    let left = currentPos.left;
-
-    switch (direction) {
-        case "Up":
-            top--;
-            break;
-        case "Down":
-            top++;
-            break;
-        case "Left":
-            left--;
-            break;
-        case "Right":
-            left++;
-            break;
-    }
-
-    let newPos = new Position(top, left, newPath);
-    newPos.setStatus(currentPos, newPos, endPos, grid);
-
-    return newPos;
-}
-
-class Position {
-    constructor(top, left, path, status = "Unknown") {
-        this.top = top;
-        this.left = left;
-        this.path = path;
-        this.status = status;
-    }
-    setStatus(currentPos, newPos, endPos, grid) {
-        const [gridWidth, gridHeight] = [grid[0].length, grid.length];
-        if (
-            newPos.left < 0 ||
-            newPos.left >= gridWidth ||
-            newPos.top < 0 ||
-            newPos.top >= gridHeight
-        ) {
-            this.status = "Invalid";
-        } else if (
-            newPos.left === endPos.left &&
-            newPos.top === endPos.top &&
-            grid[newPos.top][newPos.left].char - grid[currentPos.top][currentPos.left].char <= 1
-        ) {
-            this.status = "End";
-        } else if (
-            grid[newPos.top][newPos.left].visited ||
-            grid[newPos.top][newPos.left].char - grid[currentPos.top][currentPos.left].char > 1
-        ) {
-            this.status = "Blocked";
-        } else {
-            this.status = "Valid";
-        }
-    }
-}
-
-class Node {
-    constructor(char) {
-        this.char = char.charCodeAt();
-        this.visited = false;
-    }
-}
-
-let grid = [];
-
-let startPos = {};
-let endPos = {};
-
-for (let i = 0; i < inputs.length; i++) {
-    grid[i] = [];
-    for (let j = 0; j < inputs[i].length; j++) {
-        let char = inputs[i][j];
-        if (char === "S") {
-            startPos = { top: i, left: j };
-            char = "a";
-        } else if (char === "E") {
-            endPos = { top: i, left: j };
-            char = "z";
-        }
-        grid[i][j] = new Node(char);
-    }
-}
-
-console.log(findShortestPath(startPos, endPos, grid).length);
