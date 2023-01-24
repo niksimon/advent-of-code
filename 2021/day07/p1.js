b6565983c6034518141c158be5e996ea1db3aab4
Nikola Simonovic <nikolasimonovic94@gmail.com>
2023-01-24 01:54:11 +0100

    day 7

--- 2021/day07/p1.js
+++ 2021/day07/p1.js
@@ -1,16 +1,12 @@
 const fs = require('fs');
 const inputs = fs.readFileSync(`./input.txt`, 'utf-8').split("\r\n");
 
-const crabs = inputs[0].split(',');
+const crabs = inputs[0].split(',').map(e => +e).sort((a, b) => a - b);
+const mid = crabs[Math.floor(crabs.length / 2)];
+let fuel = 0;
 
-const [min, max] = [Math.min(...crabs), Math.max(...crabs)];
-
-const fuel = new Array(max + 1).fill(0);
-
-for(let pos = min; pos <= max; pos++) {
-    for(const crab of crabs) {
-        fuel[pos] += Math.abs(crab - pos);
-    }
+for(const crab of crabs) {
+    fuel += Math.abs(crab - mid);
 }
 
-console.log(Math.min(...fuel));
+console.log(fuel);
