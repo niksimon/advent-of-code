69c3e27295710de810f3d00a8c38b66604af96fb
Nikola Simonovic <nikolasimonovic94@gmail.com>
2023-02-02 23:00:48 +0100

    day 21

---
+++ 2021/day21/p1.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const inputs = fs.readFileSync(`./input.txt`, 'utf-8').split("\r\n");
+
+const players = [
+    {pos: +inputs[0].split(' ')[4], score: 0},
+    {pos: +inputs[1].split(' ')[4], score: 0}
+]
+
+let dice = 1;
+let timesRolled = 0;
+
+game: while(players[0].score < 1000 && players[1].score < 1000) {
+    for(const player of players) {
+        let moves = 3;
+        let nextPos = 0;
+
+        while(moves-- > 0) {
+            nextPos += dice++;
+            dice = dice > 100 ? 1 : dice;
+        }
+
+        timesRolled += 3;
+
+        player.pos = (player.pos + nextPos - 1) % 10 + 1;
+
+        player.score += player.pos;
+
+        if(player.score >= 1000) {
+            break game;
+        }
+    }
+}
+
+console.log(players[0].score > players[1].score ? players[1].score * timesRolled : players[0].score * timesRolled);
---
+++ 2021/day21/p2.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+const inputs = fs.readFileSync(`./input.txt`, 'utf-8').split("\r\n");
+
+// thanks to https://github.com/jonathanpaulson/
+
+const players = [
+    {pos: +inputs[0].split(' ')[4], score: 0},
+    {pos: +inputs[1].split(' ')[4], score: 0}
+]
+
+const cache = {};
+
+console.log(game(players));
+
+function game(players) {
+    if(players[0].score >= 21) {
+        return [1, 0];
+    }
+    if(players[1].score >= 21) {
+        return [0, 1];
+    }
+
+    const key = `${players[0].pos}:${players[1].pos}:${players[0].score}:${players[1].score}`;
+
+    if(cache[key] !== undefined) {
+        return cache[key];
+    }
+
+    const wins = [0, 0];
+    
+    for(let j = 1; j <= 3; j++) {
+        for(let k = 1; k <= 3; k++) {
+            for(let l = 1; l <= 3; l++) {
+                const newPlayer = Object.assign({}, players[0]);
+                newPlayer.pos = (newPlayer.pos + (j + k + l) - 1) % 10 + 1;
+                newPlayer.score = newPlayer.score + newPlayer.pos;
+
+                const nextWins = game([players[1], newPlayer]);
+                wins[0] += nextWins[1];
+                wins[1] += nextWins[0];
+            }
+        }
+    }
+
+    cache[key] = wins;
+
+    return wins;
+}
