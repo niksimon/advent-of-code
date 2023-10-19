01d1b2b7ddb9542ac6a40706a3a423d0789197b4
Nikola Simonovic <nikolasimonovic94@gmail.com>
2023-10-19 18:44:36 +0200

    2015 day 4

---
+++ 2015/day04/p2.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const md5 = require('md5');
+const input = fs.readFileSync(`./input.txt`, 'utf-8').split("\r\n");
+
+const key = input[0];
+let i = 1;
+let md5hash = md5(`${key}${i}`);
+
+while(!md5hash.startsWith('000000')) {
+    i++;
+    md5hash = md5(`${key}${i}`);
+}
+
+console.log(i);
