a2e76c66ce36a1d4dc49907c1d544c2e16427f69
Nikola Simonovic <nikolasimonovic94@gmail.com>
2023-12-02 10:36:11 +0100

    2023 day 1

--- 2023/day01/p2.js
+++ 2023/day01/p2.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const data = fs.readFileSync('./input.txt', 'utf-8').split('\r\n');
 
 let sum = 0;
-const map = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9};
+const map = {one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9};
 
 function findNum(line, i) {
     if(Number.isInteger(+line[i])) {
