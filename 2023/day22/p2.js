cd197715557cd69742998724a84f4f84a48fa0a1
Nikola Simonovic <nikolasimonovic94@gmail.com>
2023-12-22 21:50:16 +0100

    day 22

--- 2023/day22/p2.js
+++ 2023/day22/p2.js
@@ -4,9 +4,12 @@ const data = fs.readFileSync('./input.txt', 'utf-8').split('\r\n');
 let i = 1;
 const bricks = [];
 const bricksByLevels = {};
+const bricksByBottomLevels = {};
+let maxZ = 0;
 
 for(const line of data) {
     const [start, end] = line.split('~').map(e => { const [x, y, z] = e.split(',').map(e => +e); return {x, y, z}});
+    maxZ = Math.max(end.z, maxZ);
     bricks.push({id: i++, start, end, bricksAbove: new Set(), bricksBelow: new Set()});
 }
 
@@ -18,23 +21,45 @@ for(const brick of bricks) {
         brick.end.z--;
     }
     
+
     if(bricksByLevels[brick.end.z] === undefined) {
         bricksByLevels[brick.end.z] = [brick.id];
     }
     else {
         bricksByLevels[brick.end.z].push(brick.id);
     }
+
+    if(bricksByBottomLevels[brick.start.z] === undefined) {
+        bricksByBottomLevels[brick.start.z] = [brick.id];
+    }
+    else {
+        bricksByBottomLevels[brick.start.z].push(brick.id);
+    }
 }
 
-let disintegrate = 0;
+let sum = 0;
 for(const brick of bricks) {
     const canDisintegrate = [...brick.bricksAbove].every(b => bricks.find(e => e.id === b).bricksBelow.size > 1);
-    if(canDisintegrate) {
-        disintegrate++;
-    };
+    if(!canDisintegrate) {
+        const toDisintegrate = new Set();
+        toDisintegrate.add(brick.id);
+        let level = brick.start.z + 1;
+
+        while(level < maxZ) {
+            let bricksOnLevel = bricksByBottomLevels[level] ?? [];
+            let fallingBricks = bricksOnLevel.filter(b => [...bricks.find(e => e.id === b).bricksBelow].every(e => toDisintegrate.has(e)));
+
+            fallingBricks.forEach(e => toDisintegrate.add(e));
+    
+            level++;
+        }
+        
+        // console.log(`Brick ${brick.id} causes ${toDisintegrate.size - 1} bricks to fall`);
+        sum += toDisintegrate.size - 1;
+    }
 }
 
-console.log(disintegrate);
+console.log(sum);
 
 function collision(brick, level) {
     if(level < 1) return true;
