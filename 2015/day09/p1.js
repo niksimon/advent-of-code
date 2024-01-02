3dc2aebb9741b294b2bb8efe7b7bcb7b51658c94
Nikola Simonovic <nikolasimonovic94@gmail.com>
2024-01-02 16:18:23 +0100

    day 9

---
+++ 2015/day09/p1.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const input = fs.readFileSync(`./input.txt`, 'utf-8').split("\r\n");
+
+const map = {};
+
+for(const line of input) {
+    const [locations, distance] = line.split(' = ');
+    const [loc1, loc2] = locations.split(' to ');
+
+    if(map[loc1] === undefined) map[loc1] = {};
+    if(map[loc2] === undefined) map[loc2] = {};
+
+    map[loc1][loc2] = +distance;
+    map[loc2][loc1] = +distance;
+}
+
+const citiesCount = Object.keys(map).length;
+const stack = [...Object.keys(map).map(e => ({city: e, dist: 0, visited: [e]}))];
+let minDist = 999999;
+
+while(stack.length > 0) {
+    const current = stack.pop();
+
+    if(current.visited.length === citiesCount) {
+        minDist = Math.min(minDist, current.dist);
+    }
+
+    for(const nextCity of Object.keys(map[current.city])) {
+        if(!current.visited.includes(nextCity)) {
+            const visited = current.visited.slice();
+            visited.push(nextCity);
+            stack.push({
+                city: nextCity,
+                dist: current.dist + map[current.city][nextCity],
+                visited
+            });
+        }
+    }
+}
+
+console.log(minDist);
