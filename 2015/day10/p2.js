268d44b829964c0ad3dc007f871763f1863fc9b1
Nikola Simonovic <nikolasimonovic94@gmail.com>
2024-01-03 15:59:11 +0100

    day 10

---
+++ 2015/day10/p2.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const input = fs.readFileSync(`./input.txt`, 'utf-8').split("\r\n");
+
+let line = input[0];
+let result = '';
+
+for(let i = 0; i < 50; i++) {
+    let current = line[0];
+    let count = 1;
+    result = '';
+    
+    for(let j = 1; j < line.length; j++) {
+        if(line[j] !== current) {
+            result += `${count}${current}`;
+            count = 0;
+        }
+        current = line[j];
+        count++;
+    }
+    
+    result += `${count}${current}`;
+    line = result;
+    
+}
+
+console.log(result.length);
