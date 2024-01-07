0e1b7d239369bf634b0a2b2e26f656a6d1dd52fe
Nikola Simonovic <nikolasimonovic94@gmail.com>
2024-01-07 11:04:02 +0100

    day 16

---
+++ 2015/day16/p1.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const input = fs.readFileSync(`./input.txt`, "utf-8").split("\r\n");
+
+const gift = {
+    children: 3,
+    cats: 7,
+    samoyeds: 2,
+    pomeranians: 3,
+    akitas: 0,
+    vizslas: 0,
+    goldfish: 5,
+    trees: 3,
+    cars: 2,
+    perfumes: 1,
+};
+
+for(let i = 0; i < input.length; i++) {
+    const line = input[i];
+    const things = line.substring(line.indexOf(":") + 2, line.length)
+                        .split(', ').map(e => ({thing: e.split(': ')[0], count: +e.split(': ')[1]}));
+    
+    if(things.every(e => gift[e.thing] === e.count)) {
+        console.log(i + 1);
+    }
+}
