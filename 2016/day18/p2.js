51c2ce1044b912e90047a98e288453850fe5da37
niksimon <nikolasimonovic94@gmail.com>
2025-01-09 17:04:40 +0100

    2016 Day 18

---
+++ 2016/day18/p2.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const input = fs.readFileSync(`./input.txt`, 'utf-8').split("\r\n");
+
+let row = input[0];
+let safe = row.replaceAll('^', '').length;
+const rowCount = 400000;
+
+for(let i = 0; i < rowCount - 1; i++) {
+    let next = '';
+
+    for(let j = 0; j < row.length; j++) {
+        const prev = (row[j - 1] ?? '.') + row[j] + (row[j + 1] ?? '.');
+        if(prev == '.^^' || prev == '^^.' || prev == '^..' || prev == '..^') {
+            next += '^';
+        }
+        else {
+            next += '.';
+            safe++;
+        }
+    }
+
+    row = next;
+}
+
+console.log(safe);
